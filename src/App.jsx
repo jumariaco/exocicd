11052e32281701a63288c5d38c90c4ebbf9efb24
jumariaco <justinemariacourt@hotmail.fr>
2024-12-05 14:48:01 +0100

    feat : add Counter component above Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import Counter from './components/Counter.jsx';
 
 function App() {
   return (
     <div className="app">
       <h1 className="app-title">React Todo List</h1>
+      <Counter />
       <TodoList />
     </div>
   );
